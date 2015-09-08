09f3c6ad4e99525aa8ca06445360377ce8aed109
Daniel Swakman <dswakman@oefenweb.nl>
2015-09-08 09:37:05 +0300

    initial commit

---
+++ components/head.php
@@ -0,0 +1,18 @@
+<head>
+
+  <title>NTTD</title> 
+
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="author" content="L Daniel Swakman, www.ldaniel.eu" />
+  <meta charset="utf-8" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <link href='' rel='stylesheet' type='text/css'>
+  <link href='' rel='stylesheet' type='text/css'>
+
+  <!-- <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" /> -->
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <?php include('components/head.php'); ?>
+
+  <body>
+
+    Hello world
+    
+  </body>
+
+</html>
