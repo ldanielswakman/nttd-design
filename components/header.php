a94090871a0c22d9f3d254d679a9b268382565a8
Daniel Swakman <dswakman@oefenweb.nl>
2015-09-10 09:14:09 +0300

    intro banner & header

--- components/header.php
+++ components/header.php
@@ -1,6 +1,6 @@
 <header>
-  <a href="javascript:toggleMenu();" class="u-inlineblock u-pa10 u-floatright">
-    <div class="u-floatleft u-mt10">MENU</div>
-    <i class="icon icon-arrows-hamburger-2 icon-2x u-floatleft"></i>
+  <a href="javascript:toggleMenu();" class="u-inlineblock u-ph20 u-pv10 u-floatright">
+    <!-- <div class="u-floatleft u-mt10">MENU</div> -->
+    <i class="icon icon-arrows-hamburger-2 icon-3x u-floatleft"></i>
   </a>
 </header>
--- index.php
+++ index.php
@@ -7,18 +7,48 @@
   <body>
 
     <?php include('components/header.php'); ?>
-    <div class="header-placeholder"></div>
 
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
-    <p class="test">Expand your business to new horizons.</p>
+    <section class="slide" style="background-image: url('images/bosphorus_bridge.jpg');">
+
+      <div class="header-placeholder u-relative">
+        <a href="javascript:toggleMenu();" class="u-pintopright u-pv30 u-pr80">MENU</a>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 u-aligncenter">
+
+          <p class="test">Expand your business to new horizons.</p>
+
+          <a href="#intro" class="c-white u-inlineblock u-pa20 u-mt10"><i class="icon icon-arrows-down icon-5x"></i></a>
+
+        </div>
+      </div>
+
+      <nav class="nav-verbose u-pinbottom u-fullwidth">
+        <div class="row">
+          <div class="col-md-3 u-pv40">
+            <img src="images/holland_logo.svg" alt="" />
+          </div>
+          <div class="col-md-2 u-pv40">
+            Doing Business<br />
+            in Turkey
+          </div>
+          <div class="col-md-2 u-pv40">
+            Browse the<br />
+            business network
+          </div>
+          <div class="col-md-2 u-pv40">
+            How can we<br />
+            be of help
+          </div>
+          <div class="col-md-2 u-pv40">
+            Getting in touch
+          </div>
+        </div>
+      </nav>
     
+    </section>
+
   </body>
 
 </html>
