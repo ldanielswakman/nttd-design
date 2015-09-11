b0134d10e02214edbd9b52a8992813f0a06c1529
Daniel Swakman <dswakman@oefenweb.nl>
2015-09-11 10:13:08 +0300

    homepage intro blocks

--- components/header.php
+++ components/header.php
@@ -1,6 +1,5 @@
 <header>
   <a href="javascript:toggleMenu();" class="u-inlineblock u-ph20 u-pv10 u-floatright">
-    <!-- <div class="u-floatleft u-mt10">MENU</div> -->
     <i class="icon icon-arrows-hamburger-2 icon-3x u-floatleft"></i>
   </a>
 </header>
--- index.php
+++ index.php
@@ -22,8 +22,9 @@
 
       <div class="section-content">
 
-        <div class="header-placeholder u-relative">
-          <a href="javascript:toggleMenu();" class="u-pintopright u-pv30 u-pr80">MENU</a>
+        <div class="header-placeholder u-relative"></div>
+        <div class="u-pintopright u-pa20">
+          <a href="javascript:toggleMenu();" class="btn btn-reveal u-pr60">MENU</a>
         </div>
 
         <div class="row">
@@ -38,32 +39,37 @@
 
         <nav class="nav-verbose u-pinbottom u-fullwidth">
           <div class="row">
-            <div class="col-md-3 u-pv60">
-              <img src="images/holland_logo.svg" alt="" />
+            <div class="col-md-3">
+              <a href="/" class="u-block u-pt60 u-pb20">
+                <img src="images/holland_logo.svg" alt="" />
+              </a>
+            </div>
+            <div class="col-md-2 u-pt40">
+              <a href="business_info" class="u-block u-pb40 u-pl10 u-borderleft">
+                <i class="icon icon-arrows-expand icon-2x"></i><br />
+                <em><strong>
+                  Doing Business<br />
+                  in Turkey
+                </em></strong>
+                </em>
+              </a>
             </div>
             <div class="col-md-2 u-pv40">
-              <i class="icon icon-arrows-shrink icon-2x"></i><br />
-              <em><strong>
-                Doing Business<br />
-                in Turkey
-              </em></strong>
-            </div>
-            <div class="col-md-2 u-pv40">
-              <i class="icon icon-arrows-shrink icon-2x"></i><br />
+              <i class="icon icon-arrows-expand icon-2x"></i><br />
               <em><strong>
                 Browse the<br />
                 business network
               </em></strong>
             </div>
             <div class="col-md-2 u-pv40">
-              <i class="icon icon-arrows-shrink icon-2x"></i><br />
+              <i class="icon icon-arrows-expand icon-2x"></i><br />
               <em><strong>
                 How can we<br />
                 be of help
               </em></strong>
             </div>
             <div class="col-md-2 u-pv40">
-              <i class="icon icon-arrows-shrink icon-2x"></i><br />
+              <i class="icon icon-arrows-expand icon-2x"></i><br />
               <em><strong>
                 Questions and<br />
                 getting in touch
@@ -76,7 +82,7 @@
     
     </section>
 
-    <section id="intro" class="bg-greylight u-pv60">
+    <section id="intro" class="bg-greylight u-pv80">
       <div class="row">
         <div class="col-md-9">
           <p class="test2">Holland Turkey Trade is the online platform for Dutch companies doing business in Turkey. </p>
@@ -85,14 +91,37 @@
           <a href="javascript:void(0)" onclick="javascript:expandSection();" class="btn btn-outline u-mt20">READ MORE</a>
         </div>
       </div>
+      <div class="row u-mt40">
+        <div class="col-md-6">
+          <p><big>The site is an initiative of the Dutch Economic Diplomacy Team in Turkey with the aim to stimulate business and networking relations among the Dutch business community in Turkey.</big></p>
+        </div>
+        <div class="col-md-6">
+          <p>This initiative of the Dutch Economic Diplomacy Team that has been operational since January 2010, will help you on your way of exploring the market by sharing knowledge and information on doing business in Turkey, providing networks and showing relevant news and upcoming events from Turkey and the NL.</p>
+        </div>
+      </div>
     </section>
 
-    <section id="businessinfo" class="bg-greylight u-pv120">
+    <section id="businessinfo" class="hasSlides g-greylight u-pv120">
+
+      <div class="slides">
+        <ul>
+          <li style="background-image: url('images/maslak.jpg');"></li>
+        </ul>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="c-white u-mb40 u-aligncenter">
+            <i class="icon icon-arrows-expand icon-4x"></i>
+          </div>
+        </div>
+      </div>
+
       <div class="row">
         <div class="col-md-2">
         </div>
         <div class="col-md-9">
-          <p class="test2">
+          <p class="test2 c-white">
             Deciding to expand your business in Turkey?
             <small class="u-block u-mt20"><small>
               Our team members at the Embassy in Ankara, the Consulate in Istanbul and NBSO in Izmir are here to help you begin.
