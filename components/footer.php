e6ed9fc94c8a9d3ffc7aa38b0f2b45c740339c2e
Daniel Swakman <dswakman@oefenweb.nl>
2015-09-14 22:28:21 +0300

    footer colophon

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,12 @@
 <footer class="u-pv40">
-  <div class="row">
+  <div class="row row-nopadding">
     <div class="col-md-10 col-md-offset-1">
       <?php include('components/menu.php'); ?>
     </div>
+  </div>
+  <div class="c-greymedium u-aligncenter u-bordertop u-pt40 u-mt20">
+    &copy; 2015 [Dutch Consulate]
+    &nbsp;&nbsp;&mdash;&nbsp;&nbsp;
+    Design & front-end by <a href="http://www.ldaniel.eu/" target="_blank" class="c-bluesoft">ldaniel.eu</a> and built by <a href="http://yh.com.tr/" target="_blank" class="c-bluesoft">Yeni Hayat</a>
   </div>
 </footer>
