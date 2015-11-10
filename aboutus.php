a1a0b764623acccdb2cf58925111eef3ff6e0c5c
Daniel Swakman <dswakman@oefenweb.nl>
2015-11-10 10:18:33 +0200

    infographic stay in touch with the Dutch

--- aboutus.php
+++ aboutus.php
@@ -64,7 +64,11 @@
           <div class="content-small">
 
             <p><big><em>HollandTurkeyTrade is the online platform for Dutch companies doing business in Turkey. The site is an initiative of the Dutch Economic Diplomacy Team in Turkey.</em></big></p>
-            <p>The Team consists of the Dutch Embassy in Ankara, the Consulate General in Istanbul, the Netherlands Business Support Office (NSBO) in Izmir and Netherlands Foreign Investment Agency Istanbul. The website offers practical information on topics that may be of interest in exploring the Turkish market, starting a business in Turkey or strengthen your position in the Turkish market. Additionally HollandTurkeyTrade.com provides information on the various sectors in Turkey and the opportunities that lie ahead for Dutch companies.</p>
+            <p>The Team consists of the <a href="#embassy">Dutch Embassy in Ankara</a>, the <a href="#consulate">Consulate General in Istanbul</a>, the <a href="#support">Netherlands Business Support Office (NSBO) in Izmir</a> and Netherlands Foreign Investment Agency Istanbul. The website offers practical information on topics that may be of interest in exploring the Turkish market, starting a business in Turkey or strengthen your position in the Turkish market. Additionally HollandTurkeyTrade.com provides information on the various sectors in Turkey and the opportunities that lie ahead for Dutch companies.</p>
+
+            <figure>
+              <img src="images/content/infographic_stayintouch.png" alt="" class="u-mv30" />
+            </figure>
 
           </div>
         </div>
@@ -81,7 +85,7 @@
         <div class="col-sm-9">
           <div class="content-small">
 
-            <h3>Embassy – Ankara</h3>
+            <h3 id="embassy">Embassy – Ankara</h3>
 
             <p>The Economic Department of the Embassy in Ankara, responsible for the whole of Turkey, keeps track of the Turkish macro-economic and trade policy issues. In that capacity they maintain contacts with government authorities, the European Commission and IMF.</p>
             <p>Sector specific questions about agri-food, horticulture, defense, energy, maritime and water belong to the field of the Economic Team in Ankara. Do you have an innovative product or service that you want to introduce to the Turkish market or do you want to explore the Technology and Science sector in Turkey? Our Innovation, Technology and Science advisor will provide you of the information to accomplish this.</p>
@@ -101,7 +105,7 @@
         <div class="col-sm-9">
           <div class="content-small">
 
-            <h3>Consulate – Istanbul</h3>
+            <h3 id="consulate">Consulate – Istanbul</h3>
 
             <p>The Economic Department of the Embassy in Ankara, responsible for the whole of Turkey, keeps track of the Turkish macro-economic and trade policy issues. In that capacity they maintain contacts with government authorities, the European Commission and IMF.</p>
             <p>Sector specific questions about agri-food, horticulture, defense, energy, maritime and water belong to the field of the Economic Team in Ankara. Do you have an innovative product or service that you want to introduce to the Turkish market or do you want to explore the Technology and Science sector in Turkey? Our Innovation, Technology and Science advisor will provide you of the information to accomplish this.</p>
