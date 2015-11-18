e2f92e84b3aaffd8dbb0a545673db23c6cecd4ba
Daniel Swakman <dswakman@oefenweb.nl>
2015-11-18 06:27:23 +0200

    pagination in non-compliant html

--- components/list-pagination.php
+++ components/list-pagination.php
@@ -1,6 +1,14 @@
 <!-- Pagination -->
-<ul class="list-pagination clearfix">
-  <li>
+<div class="pagination">
+
+  <!-- Required implementation -->
+  <span class="previous_page disabled">←</span> 
+  <em class="current">1</em> 
+  <a rel="next" href="/announcements?page=2">2</a> 
+  <a class="next_page" rel="next" href="/announcements?page=2">→</a>
+
+  <!-- Standard-compliant html -->
+  <!-- <li>
     <a href="#"><i class="icon icon-arrows-left icon-15x u-floatleft"></i></a>
   </li>
   <li>
@@ -14,5 +22,6 @@
   </li>
   <li>
     <a href="#"><i class="icon icon-arrows-right icon-15x u-floatleft"></i></a>
-  </li>
-</ul>
+  </li> -->
+
+</div>
